6b4a93df3953c2bbc44b780789c09eaddc3cac01
yeet <padmasu@ymail.com>
2020-07-05 07:57:33 -0600

    Progress thus far. Don't think a dictionary will work here.

--- statement_reader.py
+++ statement_reader.py
@@ -2,27 +2,34 @@
 import pandas as pd
 import time
 import numpy as np
+import csv
+# d = {
+#      "cars":["Ford","chevy"],
+#      "food":"burgres"
+#      }
 
+# print(d)
 
-statement_data = { 'deposits': r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\deposits.csv',
-					'withdrawals': r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv' }
+infile = pd.read_csv(r"C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv")
+outfile = pd.read_csv(r"C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv")
 
-deposits = pd.read_csv(r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\deposits.csv')
-withdrawals = pd.read_csv(r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv')
+# with open(infile) as input:
+#     reader = csv.reader(infile)
+# with open(outfile) as output:
+# 	writer = csv.writer(output)
+# 	with_dict= {rows[3]:rows[4] for rows in reader}
 
-months = ['january', 'february', 'march', 'april', 'may', 'june']
-days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+statement_data = {'withdrawals': infile }
 
-if "Venmo" in withdrawals:
-	print("It's here!")
-else:
-	print("nope")
+# deposits = pd.read_csv(r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\deposits.csv')
+# withdrawals = pd.read_csv(r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv')
 
-
-
-
-
-
+# months = ['january', 'february', 'march', 'april', 'may', 'june']
+# days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+print(statement_data)
+print(statement_data['197':"Venmo"])
+#for item,cost in statement_data:
+    
 
 
 
