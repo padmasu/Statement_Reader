4d049f18093b357b488eb7176c90620ee179e3b2
yeet <padmasu@ymail.com>
2020-07-06 01:19:40 -0600

    experimenting with DataFrames

--- statement_reader.py
+++ statement_reader.py
@@ -3,37 +3,18 @@ import pandas as pd
 import time
 import numpy as np
 import csv
-# d = {
-#      "cars":["Ford","chevy"],
-#      "food":"burgres"
-#      }
 
-# print(d)
-
-infile = pd.read_csv(r"C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv")
-outfile = pd.read_csv(r"C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv")
-
-# with open(infile) as input:
-#     reader = csv.reader(infile)
-# with open(outfile) as output:
-# 	writer = csv.writer(output)
-# 	with_dict= {rows[3]:rows[4] for rows in reader}
-
-statement_data = {'withdrawals': infile }
-
-# deposits = pd.read_csv(r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\deposits.csv')
-# withdrawals = pd.read_csv(r'C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv')
-
-# months = ['january', 'february', 'march', 'april', 'may', 'june']
-# days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-print(statement_data)
-print(statement_data['197':"Venmo"])
-#for item,cost in statement_data:
-    
+def stat_reader(): 
+	infile = pd.read_csv(r"C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv")
 
 
 
+	statement_data = {'withdrawals':infile}
 
+
+	df = pd.DataFrame(data=statement_data)
+
+	print(df.to_string(index=False))
 
 
 
