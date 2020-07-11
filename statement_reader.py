20281b0ea4474d023654c4811d896f585c422ad2
yeet <padmasu@ymail.com>
2020-07-10 21:28:11 -0600

    Additional queries added.

--- statement_reader.py
+++ statement_reader.py
@@ -12,21 +12,28 @@ df['Date'] = pd.to_datetime(df['Date'])
 
 df['Day'] = df['Date'].dt.day_name()
 
+#The following converts the Amount column to Float type. Replacing comma to complete conversion. 
+df["Amount"] = df["Amount"].str.replace(',', '').astype(float)
+
 #The following line displays the most common day transactions were made.
 #It doesn't take into account the time transactions were made (Saturday night purchases technically are Sunday)
 cd = df['Day'].mode()[0]
-print("The most common day purchases were made is {}.".format(cd))
- 
- 
-#The following displays the total amount spent for each day.
+print('-'*40)
+print("The most common day purchases were made is {}.\n".format(cd))
+print('-'*40)
 
- 
- 
- 
- 
- 
- 
- 
+#The following displays the total number of transactions for each day.
+dailyc= df.Day.value_counts()
+print("\nTotal number of transactions for each day. (6 Month Period):\n{}".format(dailyc))
+print('-'*40)
+
+#The following displays the total amount spent for each day.
+amount_sum = df.groupby('Day').Amount.sum()
+print('\nTotal Amount spent for each day:\n{}'.format(amount_sum))
+
+
+
+
 # def main():
 #     while True:
      
