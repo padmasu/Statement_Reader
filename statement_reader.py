31d8ee1b23fa6234965afc21127d156bcc5289a5
yeet <padmasu@ymail.com>
2020-07-28 23:44:09 -0600

    Most recent Version. Future analysis in comments towards the bottom.

--- statement_reader.py
+++ statement_reader.py
@@ -7,7 +7,7 @@ import csv
 
 df = pd.read_csv(r"C:\Users\Phillipos Admasu\Documents\Statements\CSV\withdrawals.csv")
 df = pd.DataFrame(df).set_index('UID')
-#print(df)
+print(df)
 df['Date'] = pd.to_datetime(df['Date'])
 
 df['Day'] = df['Date'].dt.day_name()
@@ -18,31 +18,31 @@ df["Amount"] = df["Amount"].str.replace(',', '').astype(float)
 #The following line displays the most common day transactions were made.
 #It doesn't take into account the time transactions were made (Saturday night purchases technically are Sunday)
 cd = df['Day'].mode()[0]
-# print('-'*40)
-# print("The most common day purchases were made is {}.\n".format(cd))
-# print('-'*40)
+print('-'*40)
+print("\nThe most common day purchases were made is {}.\n".format(cd))
+print('-'*40)
 
 # #The following displays the total number of transactions for each day.
-# dailyc= df.Day.value_counts()
-# print("\nTotal number of transactions for each day. (6 Month Period):\n{}".format(dailyc))
-# print('-'*40)
+dailyc= df.Day.value_counts()
+print("\nTotal number of transactions for each day. (6 Month Period):\n\n{}".format(dailyc))
+print('-'*40)
 
 # #The following displays the total amount spent for each day.
-# amount_sum = df.groupby('Day').Amount.sum()
-# print('\nTotal Amount spent for each day:\n{}'.format(amount_sum))
-# print('-'*40)
+amount_sum = df.groupby('Day').Amount.sum()
+print('\nTotal Amount spent for each day:\n\n{}'.format(amount_sum))
+print('-'*40)
 
 # #The following will display which items were most costly/common.
-# totali = df.Item.value_counts()
-# print('\nThe most common item(s) purchased are:\n', totali)
+totali = df.Item.value_counts()
+print('\nThe most common item(s) purchased are:\n\n', totali)
 
-#The following will combine items which are unnecessarily split. 
-df2 = df[['Item', 'Amount']]
-df2['Item'] = df2['Item'].to_string()
-df2['Item'] = df2['Item'].str.lower()
-df2['Item_type'] = pd.Series(dtype=str)
+# #The following will combine items which are unnecessarily split. 
+# df2 = df[['Item', 'Amount']]
+# df2['Item'] = df2['Item'].to_string().lower()
+# df2['Item'] = df2['Item'].str.lower()
+# df2['Item_type'] = pd.Series(dtype=str)
 
-useri = input().lower()
-test = df2.loc[df2['Item'].str.contains(useri)]
+# useri = input().lower()
+# test = df2.loc[df2['Item'].str.contains(useri)]
 
-print(test)
+# print(test)
